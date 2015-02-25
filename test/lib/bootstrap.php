2ea3d9b7ed579d23721a6e2eab54673acbc91420
Rene Schmidt <rene@reneschmidt.de>
2015-02-25 23:00:17 +0100

    restructured dirs

--- test/lib/bootstrap.php
+++ test/lib/bootstrap.php
@@ -21,9 +21,9 @@ define('DB_DSN', 'sqlite::memory:');
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 
-require_once __DIR__ . '/../../vendor/SimpleOrm/SimpleDb.class.php';
-require_once __DIR__ . '/../../vendor/SimpleOrm/SimpleOrm.class.php';
-require_once __DIR__ . '/../../vendor/SimpleOrm/SimpleDbConfig.class.php';
-require_once __DIR__ . '/../../vendor/SimpleOrm/SampleDbConfig.class.php';
-require_once __DIR__ . '/../../vendor/SimpleOrm/SampleModel.class.php';
+require_once __DIR__ . '/../../src/SimpleDb.class.php';
+require_once __DIR__ . '/../../src/SimpleOrm.class.php';
+require_once __DIR__ . '/../../src/SimpleDbConfig.class.php';
+require_once __DIR__ . '/../../src/SampleDbConfig.class.php';
+require_once __DIR__ . '/../../src/SampleModel.class.php';
 
